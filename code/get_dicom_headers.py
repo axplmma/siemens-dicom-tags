1dfffa0f1ccc622c4a106e0a7dbb89d48220252b
Julia Marcinkowska <juliamarcinkowska98@gmail.com>
2024-09-28 22:43:20 +0100

    added save_json function

--- code/get_dicom_headers.py
+++ code/get_dicom_headers.py
@@ -1,11 +1,13 @@
+import os
+import json
 from pydicom import dcmread
-from dicom_parser import Header
 from dicom_parser.utils.siemens.csa.header import CsaHeader
 
 class dicom_siemens_scan:
 
     def __init__(self, dicom_path):
         self.dicom_path = dicom_path
+        self.filename = os.path.basename(self.dicom_path).split('.')[0]
         self.dicom_data = dcmread(self.dicom_path, stop_before_pixels=True)
     
     @property
@@ -32,12 +34,36 @@ class dicom_siemens_scan:
         i.e. either 'private' or 'CSA' headers
         """
         if self.software_version == "syngo MR E11":
-            image = Header(self.dicom_data)
-            raw_csa = image.get((0x029, 0x1110))
-            return CsaHeader(raw_csa).read()
+            raw_csa = self.dicom_data.get((0x029, 0x1110)).value
+            raw_tags = CsaHeader(raw_csa).read()
+            tags_as_dict = {key: val['value'] for key, val in raw_tags.items()}
+            return tags_as_dict
         elif self.software_version == "syngo MR XA30":
             print(f"The software version is {self.software_version}. This script is only compatible with syngo MR E11. Sorry")
             return None
         else: 
             print("The software version is not compatible with this script. Please check the software version.")
             return None
+    
+    @classmethod
+    def convert_to_serializable(self, obj):
+        if isinstance(obj, bytes):
+            return obj.decode('utf-8')
+        raise TimeoutError(f'Object of type {obj.__class__.__name__} is not JSON serializable!')
+    
+    
+    def save_json(self):
+        """
+        Save available metadata into json format.
+        Currently works for private tags and syngo MR E11 only 
+        """
+        # define json filename
+        filename = os.path.basename(self.dicom_path).split('.')[0]
+        # print(self.filename)
+        json_filename = 'sidecar_files/{}.json'.format(filename)
+        if self.software_version == "syngo MR E11":
+            # Save the simplified metadata as a JSON file
+            with open(json_filename, 'w') as f:
+                json.dump(self.public_tags, f, indent=4, default=self.convert_to_serializable)
+        else:
+            print("I don't know how to save this data to json yet :(")
