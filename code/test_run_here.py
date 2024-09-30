9b47284406b2b7c742a34410403d0bd785195383
juliam98 <juliamarcinkowska98@gmail.com>
2024-09-30 12:46:05 +0100

    added new example data

--- code/test_run_here.py
+++ code/test_run_here.py
@@ -1,15 +1,12 @@
 from get_dicom_headers import dicom_siemens_scan
 
-data_new = "data/sub-223_ses-MRI1_voi-ACC_acq-pres_svs.dcm"
-data_old = "data/sub-102_ses-MRI1_voi-ACC_acq-pres_svs.dcm"
+# OLD software version (syngo MR XA30)
+data_102 = "data/sub-102_ses-MRI1_voi-ACC_acq-pres_svs.dcm"
+participant102 = dicom_siemens_scan(data_102)
 
-participant223 = dicom_siemens_scan(data_new)
-participant102 = dicom_siemens_scan(data_old)
+# NEW software version (syngo MR E11)
+data_217 = "data/sub-217_ses-MRI1_voi-ACC_acq-pres_svs.dcm"
+participant217 = dicom_siemens_scan(data_217)
+data_223 = "data/sub-223_ses-MRI2_voi-ACC_acq-pres_svs.dcm"
+participant223 = dicom_siemens_scan(data_223)
 
-# print(participant102.software_version)
-# print(participant102.public_tags)
-# print(participant102.private_tags)
-
-# print(participant223.software_version)
-print(participant223.public_tags)
-print(participant223.private_tags)
