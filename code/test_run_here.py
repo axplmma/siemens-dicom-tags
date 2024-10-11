05fe64b4e2d567100eb81bce877b2e27e287bd2e
axplmma <49574004+axplmma@users.noreply.github.com>
2024-10-11 02:27:34 +0200

    Update test_run_here.py
    test working

--- code/test_run_here.py
+++ code/test_run_here.py
@@ -1,12 +1,17 @@
-from get_dicom_headers import dicom_siemens_scan
+from get_dicom_headers import DicomSiemensScan
 
 # OLD software version (syngo MR XA30)
 data_102 = "data/sub-102_ses-MRI1_voi-ACC_acq-pres_svs.dcm"
-participant102 = dicom_siemens_scan(data_102)
+participant102 = DicomSiemensScan(data_102)
 
 # NEW software version (syngo MR E11)
 data_217 = "data/sub-217_ses-MRI1_voi-ACC_acq-pres_svs.dcm"
-participant217 = dicom_siemens_scan(data_217)
+participant217 = DicomSiemensScan(data_217)
 data_223 = "data/sub-223_ses-MRI2_voi-ACC_acq-pres_svs.dcm"
-participant223 = dicom_siemens_scan(data_223)
+participant223 = DicomSiemensScan(data_223)
 
+targetFolder="sidecar_files"
+
+participant102.save_json(targetFolder)
+participant217.save_json(targetFolder)
+participant223.save_json(targetFolder)
